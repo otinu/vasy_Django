a63e5727d128913cf0bac3327252f42c7ec1427e
otinu <fitshter@gmail.com>
2022-08-26 13:37:26 +0900

    [add]tile

--- vasyProject/vasyApp/Scraping/kurashiru.py
+++ vasyProject/vasyApp/Scraping/kurashiru.py
@@ -16,15 +16,17 @@ for (title_image, link) in zip(titles_images, links):
     try:
         print("========================")
         print(str(i) + ".")
-        
         print(title_image.img['alt'])
         print(title_image.img['src'])
         print(URL_PATTERN + link.a['href'])
         print("========================")
         i = i + 1
-        time.sleep(1)
-
         
     except Exception as e:
         print(e)
         continue
+
+# なぜか、10番目以降が取得できない。prettify()でチラッと見た感じでは、<noscript>というタグで囲まれていた。
+# ⇒ただし、20番目あたりの話。9番目より上を見たくともコンソールに出力しきれなかった。
+# 　⇒ここの問題が解決すれば、10番目以降も取得できると思うが、今はそこまで多くのデータ取得をすることが目的ではない。
+#     ⇒後々、「もっと多くの件数を出力させたい」という場面がきた際に、再度検討。
