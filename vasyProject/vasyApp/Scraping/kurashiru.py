fdfe62825660fe21612cc7d2a2e82f1926bf8077
otinu <fitshter@gmail.com>
2022-09-03 10:00:04 +0900

    [confirm]scraping_files

--- vasyProject/vasyApp/Scraping/kurashiru.py
+++ vasyProject/vasyApp/Scraping/kurashiru.py
@@ -11,7 +11,6 @@ soup = BeautifulSoup(html_doc, 'html.parser')
 titles_images = soup.select("html > body > div > section > div > div > div > div > div > ul > li > div > a > div > div > noscript")
 links = soup.select("html > body > div > section > div > div > div > div > div > ul > li > div")
 
-"""
 i = 1
 for (title_image, link) in zip(titles_images, links):
     try:
@@ -26,9 +25,3 @@ for (title_image, link) in zip(titles_images, links):
     except Exception as e:
         print(e)
         continue
-"""
-
-# なぜか、10番目以降が取得できない。prettify()でチラッと見た感じでは、<noscript>というタグで囲まれていた。
-# ⇒ただし、20番目あたりの話。9番目より上を見たくともコンソールに出力しきれなかった。
-# 　⇒ここの問題が解決すれば、10番目以降も取得できると思うが、今はそこまで多くのデータ取得をすることが目的ではない。
-#     ⇒後々、「もっと多くの件数を出力させたい」という場面がきた際に、再度検討。
