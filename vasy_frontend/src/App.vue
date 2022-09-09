39fb441656126995373b0cf8de612e1a73f93e8c
otinu <fitshter@gmail.com>
2022-09-09 23:07:58 +0900

    [fix]arrow-bottom

--- vasy_frontend/src/App.vue
+++ vasy_frontend/src/App.vue
@@ -17,53 +17,60 @@
       crossorigin="anonymous"
     />
 
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@1,700&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@1,700&display=swap"
+      rel="stylesheet"
+    />
   </head>
   <header>
-    <h1 class="mt-2 mb-5" style="font-family: 'Cormorant Garamond', serif;"><i class="fa-solid fa-leaf text-success"></i> Vasy</h1>
+    <h1 class="mt-2 mb-5" style="font-family: 'Cormorant Garamond', serif">
+      <i class="fa-solid fa-leaf text-success"></i> Vasy
+    </h1>
   </header>
 
   <router-view />
-  <div id="pagetop" class="fixed-bottom" style="left: 87%;" v-show="scY > 300" @click="toTop">
-
-
-      <i class="fa-solid fa-circle-arrow-up fa-3x"></i>
-
-
-    </div>
+  <div
+    id="pagetop"
+    class="fixed-bottom"
+    style="left: 87%"
+    v-show="scY > 300"
+    @click="toTop"
+  >
+    <i class="fa-solid fa-circle-arrow-up fa-3x mb-2 me-5"></i>
+  </div>
 </template>
 
 <script>
 export default {
   name: "App-Component",
-  el: '#app',
-    data() {
-      return {
-        scTimer: 0,
-        scY: 0,
-      }
+  el: "#app",
+  data() {
+    return {
+      scTimer: 0,
+      scY: 0,
+    };
+  },
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll);
+  },
+  methods: {
+    handleScroll: function () {
+      if (this.scTimer) return;
+      this.scTimer = setTimeout(() => {
+        this.scY = window.scrollY;
+        clearTimeout(this.scTimer);
+        this.scTimer = 0;
+      }, 100);
     },
-    mounted() {
-      window.addEventListener('scroll', this.handleScroll);
+    toTop: function () {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
     },
-    methods: {
-      handleScroll: function () {
-        if (this.scTimer) return;
-        this.scTimer = setTimeout(() => {
-          this.scY = window.scrollY;
-          clearTimeout(this.scTimer);
-          this.scTimer = 0;
-        }, 100);
-      },
-      toTop: function () {
-        window.scrollTo({
-          top: 0,
-          behavior: "smooth"
-        });
-      },
-    },
+  },
 };
 </script>
 
@@ -89,4 +96,3 @@ nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
-
